4a523315caba1c9cd1cdcbcf7e3178db0f6d7f81
nvalembois <nvalembois@live.com>
2025-01-24 18:00:40 +0100

    Create downstream.php

---
+++ docker/downstream.php
@@ -0,0 +1,6 @@
+<?php
+define('GLPI_CONFIG_DIR',      '/data/config');
+define('GLPI_VAR_DIR',         '/data/files');
+define('GLPI_LOG_DIR',         '/data/log');
+define('GLPI_MARKETPLACE_DIR', '/data/marketplace');
+define('GLPI_DUMP_DIR',        '/data/dumps');
