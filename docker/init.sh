8a9144d7db5ac2be2f89289a173a62c9d2186901
Nicolas Valembois <nvalembois@live.com>
2025-01-25 13:27:01 +0100

    v0.1

--- docker/init.sh
+++ docker/init.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 set -e
-ls -la /data
-for d in config files log marketplace dumps
-do
-  if [[ ! -d /data/$d ]]; then
-    install -d -m 0750 /data/$d
-  fi
-done
+
+install -d -o nginx -g nginx -m 0750 /data \
+  /data/config /data/files /data/log /data/marketplace /data/dumps \
+  /data/files/_cron /data/files/_graphs /data/files/_lock \
+  /data/files/_pictures /data/files/_plugins /data/files/_rss \
+  /data/files/_sessions /data/files/_tmp /data/files/_uploads \
+  /data/files/_cache
+
+php bin/console --no-interaction glpi:system:check_requirements
+php bin/console --no-interaction db:configure \
+    --reconfigure \
+    --db-host="${MARIADB_HOST:-localhost}" \
+    --db-port="${MARIADB_PORT:-3306}" \
+    --db-name="${MARIADB_DATABASE:-glpi}" \
+    --db-user="${MARIADB_USER:-glpi}" \
+    --db-password="${MARIADB_PASSWORD:-glpi}"
+php bin/console --no-interaction db:update \
+    --${GLPI_TELEMETRY:-enable}-telemetry
