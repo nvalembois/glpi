91471ca6e789f9bdd15fe102a6547440f74df8d8
Nicolas Valembois <nvalembois@live.com>
2025-01-26 11:31:02 +0100

    multi-image refactor init fixes

--- docker/init.sh
+++ docker/init.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 set -e
 
-install -d /data \
-  /data/config /data/files /data/log /data/marketplace /data/dumps \
-  /data/files/_cron /data/files/_graphs /data/files/_lock \
-  /data/files/_pictures /data/files/_plugins /data/files/_rss \
-  /data/files/_sessions /data/files/_tmp /data/files/_uploads \
-  /data/files/_cache
+for d in config files log marketplace dumps \
+  files/_cron files/_graphs files/_lock \
+  files/_pictures files/_plugins files/_rss \
+  files/_sessions files/_tmp files/_uploads \
+  files/_cache
+do
+  [[ -d /data/$d ]] || mkdir /data/$d
+done
 
 php bin/console --no-interaction system:check_requirements
-# if [[ ! -f /data/config/config_db.php ]] # install not executed
-# then
-#   php bin/console --no-interaction db:install \
-#       --reconfigure \
-#       --db-host="${MARIADB_HOST:-localhost}" \
-#       --db-port="${MARIADB_PORT:-3306}" \
-#       --db-name="${MARIADB_DATABASE:-glpi}" \
-#       --db-user="${MARIADB_USER:-glpi}" \
-#       --db-password="${MARIADB_PASSWORD:-glpi}"
-# else
+if [[ ! -f /data/config/glpicrypt.key ]] # install not executed
+then
+  php bin/console --no-interaction db:install \
+      --reconfigure \
+      --db-host="${MARIADB_HOST:-localhost}" \
+      --db-port="${MARIADB_PORT:-3306}" \
+      --db-name="${MARIADB_DATABASE:-glpi}" \
+      --db-user="${MARIADB_USER:-glpi}" \
+      --db-password="${MARIADB_PASSWORD:-glpi}" \
+      --${GLPI_TELEMETRY:-enable}-telemetry
+else
   php bin/console --no-interaction db:configure \
       --reconfigure \
       --db-host="${MARIADB_HOST:-localhost}" \
@@ -28,5 +31,5 @@ php bin/console --no-interaction system:check_requirements
       --db-password="${MARIADB_PASSWORD:-glpi}"
   php bin/console --no-interaction db:update \
       --${GLPI_TELEMETRY:-enable}-telemetry
-# fi
+fi
 
